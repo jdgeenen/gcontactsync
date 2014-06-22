8409f1092af66ef8373c9387069332387dc0e96c
Josh Geenen <joshgeenen@gmail.com>
2014-06-22 17:58:36 -0500

    Version 0.4.0rc8

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@
 
 # NOTE: The VERSION here is just used for the package name
 # Edit install.rdf and content/synonyms.js to change the actual VERSION
-VERSION=0.4.0rc7
+VERSION=0.4.0rc8
 SRC_DIR=./src
 
 # Dest should be absolute or relative to SRC_DIR
