7c90300d7988546a31c7e04efc0827d148e6fb1f
Josh Geenen <joshgeenen@gmail.com>
2015-03-29 17:14:09 -0500

    Issue #72: Close the SyncWorker when finished

--- src/content/SyncWorker.js
+++ src/content/SyncWorker.js
@@ -169,6 +169,8 @@ function SyncWorker(aData) {
                mContactsToDelete: contactsToDelete,
                mContactsToUpdate: contactsToUpdate,
                mCardsToDelete:    cardsToDelete});
+
+  close();
 }
 
 /**
