27143577f504b8242b601fef87ae8a9c2c57ab21
Josh Geenen <joshgeenen@gmail.com>
2015-06-07 21:49:38 -0500

    Version 2.0.5

--- src/content/synonyms.js
+++ src/content/synonyms.js
@@ -49,7 +49,7 @@ com.gContactSync.versionMajor   = "2";
 /** The minor version of gContactSync (ie 3 in 0.3.0b1) */
 com.gContactSync.versionMinor   = "0";
 /** The release for the current version of gContactSync (ie 1 in 0.3.1a7) */
-com.gContactSync.versionRelease = "4";
+com.gContactSync.versionRelease = "5";
 /** The suffix for the current version of gContactSync (ie a7 for Alpha 7) */
 com.gContactSync.versionSuffix  = "";
 /** The attribute where the dummy e-mail address is stored */
