13aac958cc06b80b1fb747b67d4b59d8c2b735e5
Josh Geenen <joshgeenen@gmail.com>
2020-06-06 21:02:47 -0500

    Add version 3.2.0

--- src/content/misc.js
+++ src/content/misc.js
@@ -43,9 +43,9 @@ var {Services} = ChromeUtils.import("resource://gre/modules/Services.jsm");
 /** The major version of gContactSync (ie 0 in 0.2.18) */
 gContactSync.versionMajor   = "3";
 /** The minor version of gContactSync (ie 3 in 0.3.0b1) */
-gContactSync.versionMinor   = "1";
+gContactSync.versionMinor   = "2";
 /** The release for the current version of gContactSync (ie 1 in 0.3.1a7) */
-gContactSync.versionRelease = "7";
+gContactSync.versionRelease = "0";
 /** The suffix for the current version of gContactSync (ie a7 for Alpha 7) */
 gContactSync.versionSuffix  = "";
 /** The attribute where the dummy e-mail address is stored */
